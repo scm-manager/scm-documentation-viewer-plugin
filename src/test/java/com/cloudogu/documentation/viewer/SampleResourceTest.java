ae0a4e1fb85bf3807a7221d5d271d3ff87900a1e
Thomas Zerr <thomas.zerr@cloudogu.com>
2024-09-19 14:13:22 +0200

    Init commit

---
+++ src/test/java/com/cloudogu/documentation/viewer/SampleResourceTest.java
@@ -0,0 +1,13 @@
+package com.cloudogu.documentation.viewer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SampleResourceTest {
+
+  @Test
+  void add() {
+    assertThat(2 + 2).isEqualTo(4);
+  }
+}
